28df8314695350519e29b969856d564273854e5d
Lucy <mizkuzy@yandex.ru>
2023-12-01 22:35:25 +0200

    Add format option and two arguments: filepath1 and filepath2
    step 2

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,6 +5,11 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
+
+program
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
