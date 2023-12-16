cb4de0c4d59394d0dfe7015d647984a3192f5e61
Lucy <mizkuzy@yandex.ru>
2023-12-16 20:46:53 +0200

    implement yaml files' parsing
    step 6

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { expect, test } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -9,12 +9,16 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('gendiff', () => {
-  const file1Path = getFixturePath('plain1.json');
-  const file2Path = getFixturePath('plain2.json');
+const cases = [['json'], ['yml'], ['yaml']];
 
-  const actual = generateDifference(file1Path, file2Path);
-  const expected = fs.readFileSync(getFixturePath('plainJsonsResult'), 'utf-8');
+describe.each(cases)('gendiff for extension %s', (extension) => {
+  test('plain object', () => {
+    const file1Path = getFixturePath(`plain1.${extension}`);
+    const file2Path = getFixturePath(`plain2.${extension}`);
 
-  expect(actual).toEqual(expected);
+    const actual = generateDifference(file1Path, file2Path);
+    const expected = fs.readFileSync(getFixturePath('plainJsonsResult'), 'utf-8');
+
+    expect(actual).toEqual(expected);
+  });
 });
--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,22 @@
 import path from 'path';
 import { readFileSync } from 'fs';
+import yaml from 'js-yaml';
 
 const parseFile = (filePath) => {
+  const extension = path.extname(filePath);
+
   const fp = path.resolve(filePath);
   const objAsString = readFileSync(fp, { encoding: 'utf8', flag: 'rs' });
 
-  return JSON.parse(objAsString);
+  switch (extension) {
+    case '.json':
+      return JSON.parse(objAsString);
+    case '.yml':
+    case '.yaml':
+      return yaml.load(objAsString);
+    default:
+      throw new Error(`Unsupported extension ${extension}`);
+  }
 };
 
 const parseFiles = (...args) => args.map(parseFile);
