cc318cdf7a2f1bec58115a5c169f911430063a74
Lucy <mizkuzy@yandex.ru>
2023-12-17 20:11:54 +0200

    implement nested objects processing
    step 7

--- src/formatter.js
+++ src/formatter.js
@@ -40,31 +40,12 @@ const getStyledDiff = ({ diffAddedValues, diffRemovedValues, ...rest }) => {
     }
   });
 
-  console.log('lines', lines);
   return [
     '{',
     ...lines,
     '}',
   ].join('\n');
 };
-//
-// const diffObjExample = {
-//   common: {
-//     _addedValues: {
-//       follow: false, setting3: null, setting4: 'blah blah', setting5: { key5: 'value5' },
-//     },
-//     _removedValues: { setting2: 200, setting3: true },
-//     setting1: 'Value 1',
-//     setting6: {
-//       _added_values: { ops: 'vops' },
-//       doge: {
-//         _added_values: { wow: 'so much' },
-//         _removed_values: { wow: '' },
-//       },
-//       key: 'value',
-//     },
-//   },
-// };
 
 const formatDiff = (diff, type) => {
   if (type === 'stylish') {
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,29 +1,74 @@
 import { Command } from 'commander';
+import isObject from 'lodash.isobject';
+import uniq from 'lodash.uniq';
 import parseFiles from './parser.js';
 import formatDiff from './formatter.js';
 
-const getDiff = (object1, object2) => {
-  const keys = [...Object.keys(object1), ...Object.keys(object2)];
+// const diffObjExample = {
+//   common: {
+//     diffAddedValues: {
+//       follow: false, setting3: null, setting4: 'blah blah', setting5: { key5: 'value5' },
+//     },
+//     diffRemovedValues: { setting2: 200, setting3: true },
+//     setting1: 'Value 1',
+//     setting6: {
+//       diffAddedValues: { ops: 'vops' },
+//       doge: {
+//         diffAddedValues: { wow: 'so much' },
+//         diffRemovedValues: { wow: '' },
+//       },
+//       key: 'value',
+//     },
+//   },
+//   group1: {
+//     foo: 'bar',
+//     diffAddedValues: { baz: 'bars', nest: 'str' },
+//     diffRemovedValues: { baz: 'bas', nest: { key: 'value' } },
+//   },
+//   diffAddedValues: { myTest: 1 },
+//   diffRemovedValues: { myTest: { inner: 1 } },
+// };
 
-  const result = {};
+const getDiff = (inputObj1, inputObj2) => {
+  console.log('!!GET_DIFF');
+  const iter = (acc, object1, object2) => {
+    console.log('object1', object1);
+    console.log('object2', object2);
 
-  keys.forEach((key) => {
-    const val1 = object1[key];
-    const val2 = object2[key];
+    const keys = [...Object.keys(object1), ...Object.keys(object2)];
+    const uniqueKeys = uniq(keys);
 
-    if (val1 === val2) {
-      result[key] = val1;
-    } else if (val1 === undefined) {
-      result.diffAddedValues = { ...result.diffAddedValues, [key]: val2 };
-    } else if (val2 === undefined) {
-      result.diffRemovedValues = { ...result.diffRemovedValues, [key]: val1 };
-    } else {
-      result.diffAddedValues = { ...result.diffAddedValues, [key]: val2 };
-      result.diffRemovedValues = { ...result.diffRemovedValues, [key]: val1 };
-    }
-  });
+    uniqueKeys.forEach((key) => {
+      const val1 = object1[key];
+      const val2 = object2[key];
 
-  return result;
+      // case both objects
+      if (isObject(val1) && isObject(val2)) {
+        acc[key] = iter({}, val1, val2);
+      } else if (
+        // case one object, one primitive
+        (val2 !== undefined && isObject(val1) && !isObject(val2))
+        || (val1 !== undefined && isObject(val2) && !isObject(val1))
+      ) {
+        acc.diffAddedValues = { ...acc.diffAddedValues, [key]: val2 };
+        acc.diffRemovedValues = { ...acc.diffRemovedValues, [key]: val1 };
+      } else if (val1 === val2) {
+      // case both primitives
+        acc[key] = val1;
+      } else if (val1 === undefined) {
+        acc.diffAddedValues = { ...acc.diffAddedValues, [key]: val2 };
+      } else if (val2 === undefined) {
+        acc.diffRemovedValues = { ...acc.diffRemovedValues, [key]: val1 };
+      } else {
+        acc.diffAddedValues = { ...acc.diffAddedValues, [key]: val2 };
+        acc.diffRemovedValues = { ...acc.diffRemovedValues, [key]: val1 };
+      }
+    });
+
+    return acc;
+  };
+
+  return iter({}, inputObj1, inputObj2);
 };
 
 const generateDifference = (fp1, fp2, formatType = 'stylish') => {
