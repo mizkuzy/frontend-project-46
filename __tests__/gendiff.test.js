fb7c38c7e6a188c2c348ebee54cad9681ca206a9
Lucy <mizkuzy@yandex.ru>
2023-12-17 15:34:06 +0200

    implement test for files with nested structure
    step 7

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,6 +8,7 @@ import { generateDifference } from '../src/gendiff.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const cases = [['json'], ['yml'], ['yaml']];
 
@@ -17,7 +18,17 @@ describe.each(cases)('gendiff for extension %s', (extension) => {
     const file2Path = getFixturePath(`plain2.${extension}`);
 
     const actual = generateDifference(file1Path, file2Path);
-    const expected = fs.readFileSync(getFixturePath('plainJsonsResult'), 'utf-8');
+    const expected = readFile('plainJsonsResult');
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('nested object', () => {
+    const file1Path = getFixturePath(`nested1.${extension}`);
+    const file2Path = getFixturePath(`nested2.${extension}`);
+
+    const actual = generateDifference(file1Path, file2Path);
+    const expected = readFile('nestedJsonsResult');
 
     expect(actual).toEqual(expected);
   });
