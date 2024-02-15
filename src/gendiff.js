fe8f730d2e534f830f7c5dafdab296ac96cdd0ac
Lucy <mizkuzy@yandex.ru>
2024-02-15 20:11:17 +0200

    fix getDiff

--- src/gendiff.js
+++ src/gendiff.js
@@ -4,39 +4,57 @@ import parseFiles from './parser.js';
 import getFormattedDiff from './formatters/index.js';
 
 const getDiff = (inputObj1, inputObj2) => {
-  const iter = (acc, object1, object2) => {
+  const iter = (diffObject, object1, object2) => {
     const keys = [...Object.keys(object1), ...Object.keys(object2)];
     const uniqueKeys = uniq(keys);
 
-    const newAcc = { ...acc };
-    uniqueKeys.forEach((key) => {
+    return uniqueKeys.reduce((accumulator, key) => {
       const val1 = object1[key];
       const val2 = object2[key];
 
       // case both objects
       if (isObject(val1) && isObject(val2)) {
-        newAcc[key] = iter({}, val1, val2);
-      } else if (
+        return {
+          ...accumulator, [key]: iter({}, val1, val2),
+        };
+      }
+      if (
         // case one object, one primitive
         (val2 !== undefined && isObject(val1) && !isObject(val2))
         || (val1 !== undefined && isObject(val2) && !isObject(val1))
       ) {
-        newAcc.diffAddedProperties = { ...newAcc.diffAddedProperties, [key]: val2 };
-        newAcc.diffRemovedProperties = { ...newAcc.diffRemovedProperties, [key]: val1 };
-      } else if (val1 === val2) {
-      // case both primitives
-        newAcc[key] = val1;
-      } else if (val1 === undefined) {
-        newAcc.diffAddedProperties = { ...newAcc.diffAddedProperties, [key]: val2 };
-      } else if (val2 === undefined) {
-        newAcc.diffRemovedProperties = { ...newAcc.diffRemovedProperties, [key]: val1 };
-      } else {
-        newAcc.diffAddedProperties = { ...newAcc.diffAddedProperties, [key]: val2 };
-        newAcc.diffRemovedProperties = { ...newAcc.diffRemovedProperties, [key]: val1 };
+        return {
+          ...accumulator,
+          diffAddedProperties: { ...accumulator.diffAddedProperties, [key]: val2 },
+          diffRemovedProperties: { ...accumulator.diffRemovedProperties, [key]: val1 },
+        };
       }
-    });
+      if (val1 === val2) {
+        // case both primitives
+        return {
+          ...accumulator,
+          [key]: val1,
+        };
+      }
+      if (val1 === undefined) {
+        return {
+          ...accumulator,
+          diffAddedProperties: { ...accumulator.diffAddedProperties, [key]: val2 },
+        };
+      }
+      if (val2 === undefined) {
+        return {
+          ...accumulator,
+          diffRemovedProperties: { ...accumulator.diffRemovedProperties, [key]: val1 },
+        };
+      }
 
-    return newAcc;
+      return {
+        ...accumulator,
+        diffAddedProperties: { ...accumulator.diffAddedProperties, [key]: val2 },
+        diffRemovedProperties: { ...accumulator.diffRemovedProperties, [key]: val1 },
+      };
+    }, diffObject);
   };
 
   return iter({}, inputObj1, inputObj2);
